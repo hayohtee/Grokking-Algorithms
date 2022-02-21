ee6ec6f874238b2afae69edcb9c28e03472a096a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2022-02-21 21:48:16 +0100

    Created a folder Chapter 4 and the implementation for the divide and conquer

---
+++ Chapter 4/divide_and_conquer.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+// Returns the sum of the elements of the
+// vector using divide and conquer.
+int sum(std::vector<int> arr);
+
+int main()
+{
+    std::vector<int> arr = {2, 4, 6};
+
+    std::cout << "The sum of elements in the vector is: " << sum(arr)
+              << std::endl;
+
+    return 0;
+}
+
+int sum(std::vector<int> arr)
+{
+    if (arr.size() == 1)
+        return arr[0];
+    
+    int firstElement = arr[0];
+    arr.erase(arr.begin()); // remove the first element
+
+    return (firstElement + sum(arr));
+}
